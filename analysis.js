68231be94734c41c331992bb0f020c29e603a1c4
Ilari <ilari.ristikankare@aalto.fi>
2022-05-18 17:01:07 +0300

    Final commit 2 feat like button - "Axel F"

--- analysis.js
+++ analysis.js
@@ -1,9 +1,30 @@
-const AddComment = () => {
+const commentContainer = document.getElementById("allComments");
 
-  var comment = document.getElementById('comment_id').value;
-  var newP = document.createElement("p");
-  var content = document.createTextNode(comment);
-  newP.appendChild(content);
-  document.getElementById('div_id').appendChild(newP);
+function addComment() {
+  const textBox = document.createElement("p");
+  textBox.className = "comment-text";
+  const likeButton = document.createElement("button");
+  likeButton.innerHTML = "Like";
+  likeButton.className = "like-button";
+  const wrapDiv = document.createElement("div");
+  wrapDiv.className = "wrapper";
+  wrapDiv.style.marginLeft = 0;
+  commentText = document.getElementById("newComment").value;
+  document.getElementById("newComment").value = "";
+  textBox.innerHTML = commentText;
+  wrapDiv.append(textBox, likeButton);
+  if (commentText === "") {
+    alert("Comment field can't be empty");
+  } else commentContainer.appendChild(wrapDiv);
+}
 
-};
+function hasClass(elem, className) {
+  return elem.className.split(" ").indexOf(className) > -1;
+}
+document.getElementById("allComments").addEventListener("click", function (e) {
+  if (hasClass(e.target, "like-button")) {
+    const likeBtnValue = e.target.innerHTML;
+    e.target.innerHTML =
+      likeBtnValue !== "Like" ? Number.parseInt(likeBtnValue) + 1 : 1;
+  }
+});
